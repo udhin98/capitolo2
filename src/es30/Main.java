15c93bce4dbae7214ab3be5166857f447ac5674c
Pritvi <udhin1998@gmail.com>
2023-11-06 11:04:15 +0100

    es30

---
+++ src/es30/Main.java
@@ -0,0 +1,9 @@
+package es30;
+
+public class Main {
+    public static void main(String[] args) {
+        Auto auto1 = new Auto(220, "BA12EZY", "Fiat", "Punto");
+        System.out.println(auto1);
+    }
+
+}
