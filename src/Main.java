2fe1995b74606815c18be25c918ce60b573d6447
Pritvi <udhin1998@gmail.com>
2023-11-06 10:47:13 +0100

    es29

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        Studente studente1 = new Studente("Pritvi", "Udhin", 2801);
+        System.out.println(studente1);
 
         }
+
     }
--- src/Studente.java
+++ src/Studente.java
@@ -1,2 +1,45 @@
 public class Studente {
+    private String nome;
+    private String cognome;
+    private int id;
+
+    public Studente(String nome, String cognome, int id)
+    {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCognome()
+    {
+        return cognome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Studente{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
